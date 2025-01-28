d52b2fa33fb56a42cb4cd01a5f32b78b56837441
Rowan Mankoo <rowan.mankoo@gmail.com>
2025-01-28 15:34:38 +0000

    trigger CICD test

--- infra/tf/modules/cloud_run/outputs.tf
+++ infra/tf/modules/cloud_run/outputs.tf
@@ -3,6 +3,7 @@ output "instance_id" {
   value       = google_compute_instance.vm_instance.id
 }
 
+# test
 output "instance_ip" {
   description = "The public IP of the Compute Engine instance"
   value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
