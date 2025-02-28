32cbbbcf881cc97abcdae0cac1420aafb5320756
Rowan Mankoo <rowan.mankoo@gmail.com>
2025-02-28 00:07:45 +0000

    test removal of cloud_run_job

--- infra/tf/cloud_run_jobs.tf
+++ infra/tf/cloud_run_jobs.tf
@@ -1,6 +1,6 @@
-module "create_reddit_stories_job" {
-  source                = "./modules/cloud_run"
-  image                 = var.cloud_run_job_image__generate_reddit_story_videos
-  service_account_email = google_service_account.cloud_run_job_service_account.email
+# module "create_reddit_stories_job" {
+#   source                = "./modules/cloud_run"
+#   image                 = var.cloud_run_job_image__generate_reddit_story_videos
+#   service_account_email = google_service_account.cloud_run_job_service_account.email
 
-}
+# }
